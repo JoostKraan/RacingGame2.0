22731fb3d1ee32f835982be222b133815fafa2e2
Walter_van_der_Broek <wbvanderbroek@st.noorderpoort.nl>
2023-01-20 15:12:11 +0100

    checkpoints werkende

--- Assets/Scripts/playerCheckpoints.cs
+++ Assets/Scripts/playerCheckpoints.cs
@@ -14,8 +14,12 @@ public class playerCheckpoints : MonoBehaviour
         car.checkPoints[car.checkPointCounter].GetComponent<MeshRenderer>().enabled = true;
         if (car.checkPointCounter == 0)
         {
-            car.checkPoints.Length; 
-            car.checkPoints[car.checkPointCounter].GetComponent<MeshRenderer>().enabled = false;
+            if (car.checkPoints[car.checkPoints.Length-1].GetComponent<MeshRenderer>().enabled == true)
+            {
+                car.checkPoints[car.checkPoints.Length - 1].GetComponent<MeshRenderer>().enabled = false;
+            }
+            //car.checkPoints.Length; 
+            //car.checkPoints[car.checkPointCounter].GetComponent<MeshRenderer>().enabled = false;
             print("0");
         }
         else
