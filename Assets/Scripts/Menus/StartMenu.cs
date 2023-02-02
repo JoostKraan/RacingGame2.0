05b224566ee5f47b1c95ed167fe1ae0a1fcf6493
wbvanderbroek <wbvanderbroek@st.noorderpoort.nl>
2023-02-02 19:25:23 +0100

    name shit

--- Assets/Scripts/Menus/StartMenu.cs
+++ Assets/Scripts/Menus/StartMenu.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartMenu : MonoBehaviour
 {
+    public TMP_InputField nameInput;
     public void Track1()
     {
         SceneManager.LoadScene(2);
@@ -15,6 +19,9 @@ public class StartMenu : MonoBehaviour
     }
     public void StartButton()
     {
+        string playerName = nameInput.text;
+        PlayerPrefs.SetString("playerName", playerName);
+        PlayerPrefs.Save();
         SceneManager.LoadScene(1);
     }
 }
